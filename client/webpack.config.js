29fe689b5614a82f4caae2b4570bfad488521906
MahmoudAboelazm <ma7moudaboelazm@gmail.com>
2022-09-26 15:46:29 +0200

    Add webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./public",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
