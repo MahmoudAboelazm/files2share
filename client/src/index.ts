262114378f89f33bfde1dba619a1ae7ab9e6437a
MahmoudAboelazm <ma7moudaboelazm@gmail.com>
2022-09-28 21:06:18 +0200

    Add index.ts & types

---
+++ client/src/index.ts
@@ -0,0 +1,5 @@
+import UIManager from "./ui/UIManager";
+
+const ui = new UIManager();
+ui.init();
+ui.serverConnect();
---
+++ client/src/types.ts
@@ -0,0 +1,75 @@
+import { Observable } from "./lib/Observable";
+import { Peer } from "./lib/Peer";
+
+export interface OnEvent {
+  [k: string]: Function;
+}
+
+export interface UIPeer {
+  peer: Peer;
+}
+
+export type Signal = (signal: SignalType) => any;
+
+export interface SignalType {
+  sdp: RTCSessionDescription | null;
+  candidate: RTCIceCandidate;
+}
+
+export interface Options {
+  initiator: boolean;
+}
+
+export interface Device {
+  name: string;
+  model: string | undefined;
+  os: string | undefined;
+  browser: string | undefined;
+  type: string | undefined;
+  vendor: string | undefined;
+  imgURL: string;
+}
+export interface ServerDevice {
+  id: string;
+  device: Device;
+}
+export interface Devices {
+  devices: ServerDevice[];
+}
+
+export interface CreateSignal {
+  signal?: SignalType;
+  send: Function;
+}
+
+export interface ServerSignal {
+  peer: {
+    id: string;
+    signal: SignalType;
+  };
+}
+
+export interface FileReceived {
+  name: string;
+  mime: string;
+  size: number;
+  blob: Blob;
+}
+export interface DeviceManagerOptions {
+  initiator: boolean;
+  device: Device;
+  id: string;
+  duplicates: Observable;
+  busy: Observable;
+}
+
+export interface FileMeta {
+  size: number;
+  mime: string;
+  name: string;
+}
+
+export interface UseLocalStorage {
+  name: string;
+  value?: string;
+}
