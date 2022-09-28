47f1340ac6ffb85b8fba9b907b28969892e49f68
MahmoudAboelazm <ma7moudaboelazm@gmail.com>
2022-09-28 21:03:51 +0200

    Add UIManager & theme - UIManager to manage all the devices connection and the main ui of my device

---
+++ client/src/ui/UIManager.ts
@@ -0,0 +1,118 @@
+import { DeviceManager } from "./deviceManager";
+import { Observable } from "../lib/Observable";
+import ServerConnection from "../lib/ServerConnection";
+import { addBackground, getTheme } from "./theme";
+import {
+  Device,
+  Devices,
+  ServerDevice,
+  ServerSignal,
+  SignalType,
+} from "../types";
+
+export default class UIManager {
+  private devices: Map<string, DeviceManager>;
+  private server: ServerConnection;
+  private preventDuplicates: Observable;
+  private myDeviceDom: Element;
+  private deviceBusy: Observable;
+
+  constructor() {
+    this.devices = new Map();
+    this.myDeviceDom = document.getElementsByClassName("my-device")[0];
+    this.preventDuplicates = new Observable();
+    this.deviceBusy = new Observable();
+  }
+
+  serverConnect() {
+    const url = process.env.URL as string;
+    const server = new ServerConnection(url);
+    server.on("network-devices", (data: Devices) => this.networkDevices(data));
+    server.on("new-device", (data: ServerDevice) => this.newDevice(data));
+    server.on("peer-signal", (data: ServerSignal) => this.peerSignal(data));
+    server.on("device-left", ({ id }) => this.deviceLeft(id));
+    server.on("my-device", ({ device }) => this.myDevice(device));
+    this.server = server;
+  }
+
+  init() {
+    getTheme();
+    addBackground();
+    this.preventDuplicateOnChange();
+    this.serviceWorker();
+  }
+
+  private serviceWorker() {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then(() => console.log("Service Worker registered"));
+    }
+  }
+
+  private preventDuplicateOnChange() {
+    const input = this.myDeviceDom.children[2];
+    const selectedLength = this.myDeviceDom.children[4];
+    input.addEventListener("change", (e) => {
+      const input = e.target as HTMLInputElement;
+      const files = input.files;
+      if (files) {
+        const filesMap = new Map<string, boolean>();
+        for (let i = 0; i < files.length; i++) {
+          filesMap.set(files[i].name, true);
+        }
+        this.preventDuplicates.next(filesMap);
+        selectedLength.innerHTML = `${files.length || 0} Seletcted File`;
+      }
+      input.value = null as any;
+    });
+  }
+
+  private myDevice(device: Device) {
+    const img = this.myDeviceDom.children[0].children[0] as HTMLImageElement;
+    const name = this.myDeviceDom.children[1] as HTMLElement;
+    img.src = device.imgURL;
+    name.innerText = device.name;
+  }
+
+  //// network devices connection ////////////////////////////////////////////////
+  private networkDevices({ devices }: Devices) {
+    devices.forEach(({ id, device }) => {
+      const deviceManager = new DeviceManager({
+        initiator: false,
+        device,
+        id,
+        duplicates: this.preventDuplicates,
+        busy: this.deviceBusy,
+      });
+      this.devices.set(id, deviceManager);
+    });
+  }
+
+  private newDevice({ device, id }: ServerDevice) {
+    const config = {
+      initiator: true,
+      device,
+      id,
+      duplicates: this.preventDuplicates,
+      busy: this.deviceBusy,
+    };
+    const deviceManager = new DeviceManager(config);
+    deviceManager.createSignal({
+      send: (signal: SignalType) => this.server.send(signal),
+    });
+    this.devices.set(id, deviceManager);
+  }
+
+  private peerSignal({ peer }: ServerSignal) {
+    this.devices.get(peer.id)?.updateSignal({
+      send: (signal: SignalType) => this.server.send(signal),
+      signal: peer.signal,
+    });
+  }
+
+  private deviceLeft(id: string) {
+    this.devices.get(id)?.destroy();
+    this.devices.delete(id);
+  }
+}
---
+++ client/src/ui/theme.ts
@@ -0,0 +1,55 @@
+import { UseLocalStorage } from "../types";
+
+export const useLocalStorage = ({ name, value }: UseLocalStorage) => {
+  if (value) {
+    localStorage.setItem(name, value);
+    return value;
+  }
+
+  const jsonValue = localStorage.getItem(name);
+  if (jsonValue) return jsonValue;
+
+  return "";
+};
+
+export const getTheme = () => {
+  const theme = useLocalStorage({ name: "theme" });
+  if (
+    theme === "dark" ||
+    (!theme && window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.body.classList.add("dark");
+  }
+  document
+    .querySelector("#change-theme")
+    ?.addEventListener("click", toggleTheme);
+};
+
+const toggleTheme = () => {
+  const theme = useLocalStorage({ name: "theme" });
+  if (theme === "dark") {
+    document.body.classList.remove("dark");
+    return useLocalStorage({ name: "theme", value: "light" });
+  }
+  document.body.classList.add("dark");
+  return useLocalStorage({ name: "theme", value: "dark" });
+};
+
+///////////////////////////////////////////////
+export const addBackground = () => {
+  const sonarContainer = document.getElementsByClassName("sonar-container")[0];
+  const vw = window.innerWidth;
+  const vh = window.innerHeight;
+
+  const sonarMaxWidth = vh > vw ? vh : vw;
+  let currentWidth = 90;
+
+  while (currentWidth < sonarMaxWidth) {
+    const span = document.createElement("span");
+    span.classList.add("sonar", "center");
+    span.style.width = currentWidth + "px";
+    span.style.height = currentWidth + "px";
+    sonarContainer.appendChild(span);
+    currentWidth += vh > vw ? 100 : 200;
+  }
+};
